cc8dd7ff0590ed0d169ec3e021ed6c1911069f37
Salomão Ribeiro <salomaomdm@gmail.com>
2022-10-31 07:27:12 -0300

    Criada a tela de eventos usando interpolação, diretiva e binding

--- ProEventos.App/src/app/eventos/eventos.component.ts
+++ ProEventos.App/src/app/eventos/eventos.component.ts
@@ -8,20 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  private _filtroLista: string = '';
+  public _todosEventos: any = [];
+  public eventos: any = [];
+  imageWidth: number = 150;
+  imageMargin: number = 2;
+  mostrarImagens: boolean = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
 
+  public get filtroLista()
+  {
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string)
+  {
+    this._filtroLista = value;
+    this.eventos = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this._todosEventos;
+  }
+
+  private filtrarEventos(filtro: string) : any
+  {
+    filtro = filtro.toLocaleLowerCase();
+    return this._todosEventos.filter(
+      (evento: {local: string ; tema: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtro) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtro) !== -1
+    )
   }
 
   public getEventos(){
     this.http.get("http://localhost:5268/api/eventos").subscribe(
-      response => this.eventos = response,
+      response => {
+        this._todosEventos = response;
+        this.eventos = response;
+      },
       error => console.log(error)
     );
+  }
+
+  public mostrarImagem()
+  {
+    this.mostrarImagens = !this.mostrarImagens;
   }
 
 }
